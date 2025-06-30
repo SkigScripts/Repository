b24f13b3b1f34f700bdaa751291390b07a19437c
SkigScripts <sdforrorar@gmail.com>
2025-06-30 12:48:46 +0300

    Add files via upload

---
+++ Mad-City.lua
@@ -0,0 +1,17 @@
+getgenv().ProBaconHubMadCityChapter2AutoRobV3Settings = {
+    Licence = "FREEMIUM_LICENCE_zkjT,1=GB,h};Y=",
+    MiniHeist = true,
+    -- Only users with a valid licence key will be able to activate the options below.
+    Bank = true,
+    Boat = false,
+    Club = true,
+    Jewelry = true,
+    Plane = false,
+    Pyramid = true,
+    Resort = true,
+    SpeedMode = true,
+    Webhook = "", -- Your discord webhook
+    DiscordId = "https://discord.gg/Zzqhc9uT", -- Your discord userid
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ProBaconHub/Mad-City-Script/refs/heads/main/MadCityAutoRobV3"))()
